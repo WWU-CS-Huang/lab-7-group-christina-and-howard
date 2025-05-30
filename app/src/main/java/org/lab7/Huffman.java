4dfbf2847a0b371c6a846fa73d8620d3d6795076
Howard Walz <walzh@wwu.edu>
2025-05-30 15:12:15 -0700

    Added method specifications for each required method.

--- app/src/main/java/org/lab7/Huffman.java
+++ app/src/main/java/org/lab7/Huffman.java
@@ -1,8 +1,39 @@
 package lab7;
 
+import java.lang.UnsupportedOperationException;
+import avl.AVL;
+import heap.HashTable;
+
 public class Huffman {
 
   public static void main(String[] args) {
+    // TODO: update the match Lab spec (will do last).
     System.out.println("Huffman");
+  }
+
+  /** countFrequencies - Counts the frequencies of each charecter in the string.
+    * stores the results in a HashTable with Strings as keys and  Integers as values.
+    */
+  public static HashTable<String, Integer> countFrequencies(String input) {
+    // TODO: Christina implement this.
+    throw new UnsupportedOperationException();
+  }
+
+  /** buildTree - builds a Huffman tree for the given frequency Hashmap. */
+  public static AVL buildTree(HashTable<String, Integer> freq) {
+    // TODO: Howard implement this.
+    throw new UnsupportedOperationException();
+  }
+
+  /** decode - uses given tree to decode the given bitstring. */
+  public static String decode(AVL tree, String bitString) {
+    // TODO: Christina implement this.
+    throw new UnsupportedOperationException();
+  }
+
+  /** encode - uses the given tree to encode the given input string. */
+  public static String encode(AVL tree, String input) {
+    // TODO: Howard implement this.
+    throw new UnsupportedOperationException();
   }
 }
