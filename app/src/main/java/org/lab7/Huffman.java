67927355e51a128acfcd371095de80c342e65ed3
Howard Walz <walzh@wwu.edu>
2025-06-06 00:50:51 -0700

    Main is set up properly just need to make everything actually work now.

--- app/src/main/java/org/lab7/Huffman.java
+++ app/src/main/java/org/lab7/Huffman.java
@@ -8,65 +8,43 @@ import java.util.HashMap;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 import java.lang.StringBuilder;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
 
 public class Huffman {
 
   /** filenames are relative to the app directory */
-  public static void main(String[] args) {
-    String input = "bee nuts.!!!!!";
-    HashMap<Character, Integer> dummyHash = new HashMap<Character, Integer>();
-    dummyHash.put('b', 1);
-    dummyHash.put('e', 2);
-    dummyHash.put(' ', 1);
-    dummyHash.put('n', 1);
-    dummyHash.put('u', 1);
-    dummyHash.put('t', 1);
-    dummyHash.put('s', 1);
-    dummyHash.put('.', 1);
-    dummyHash.put('!', 5);
+  public static void main(String[] args) throws IOException{
+    String directory = "src/main/resources/";
+    File fileLocation = new File(directory + args[0]);
+    String input = Files.readString(fileLocation.toPath());
+    HashMap<Character, Integer> dummyHash = countFrequencies(input);
+
     HuffmanNode root = buildTree(dummyHash);
-    HuffmanNode.printSubtree(root, 0);
-    System.out.println("==========================================");
+    //HuffmanNode.printSubtree(root, 0);
+    //System.out.println("==================");
     String bitString = encode(root, input);
-    System.out.println("Encoded String: " + bitString);
     String decoded = decode(root, bitString);
-    System.out.println("Decoded String: " + decoded);
+    
+    if (input.length() < 100) {
+      System.out.println("Input: " + input);
+      System.out.println("Encoded String: " + bitString);
+      System.out.println("Decoded String: " + decoded);
+    }
+
+    System.out.println("Equality: " + input.equals(decoded));
+    System.out.println("Compression Ratio: " + bitString.length() / input.length() / 8.0);
   }
 
   /** countFrequencies - Counts the frequencies of each charecter in the string.
     * stores the results in a HashTable with Strings as keys and  Integers as values.
     */
-  private static HashMap<Character, Integer> countFrequencies(String fileName) {
-    // Christina Part 1
-
-    // Use to create and access the text file
-    File fileToTally = new File(fileName);
-    String fileAsString = fileToTally.toString();
-
-    // Create Hashmap to hash individual chars into
+  private static HashMap<Character, Integer> countFrequencies(String input) {
     HashMap<Character, Integer> hash = new HashMap<Character, Integer>();
-    char inputChar;
-    
-    // Go through entire file
-    for (int i = 0; i < fileToTally.length(); i++) {
 
-      // Get next input, and hash it to where it needs to go
-      inputChar = fileAsString.charAt(i);
-
-      // If existing in the hashmap, increment value
-      if (hash.containsKey(inputChar)) {
-        hash.put(inputChar, hash.get(inputChar) + 1);
-      } 
-      
-      // Otherwise create a new entry
-      else if (hash.get(inputChar) == null) {
-        hash.put(inputChar, 1);
-      } 
-      
-      // ???
-      else {
-        throw new UnsupportedOperationException(); 
-      }
+    for (char c : input.toCharArray()) {
+      hash.put(c, hash.get(c) == null ? 1 : hash.get(c) + 1);
     }
 
     return hash;
@@ -124,7 +102,9 @@ public class Huffman {
     StringBuilder sb = new StringBuilder();
     for(char c : input.toCharArray()) {
       Integer current = bitTable.get(Character.toString(c));
-      sb.append(Integer.toBinaryString(current));
+      if (current != null) {
+	sb.append(Integer.toBinaryString(current));
+      }
     }
     return sb.toString();
   }
